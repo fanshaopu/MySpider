7840b353b8e87d382f1e78c0d77d19f7048375cc
fanshaopu <fanshaopubit@163.com>
2015-10-12 10:52:01 +0800

    Create Pageprocessor.py

---
+++ Pageprocessor.py
@@ -0,0 +1,10 @@
+#-*- coding:utf-8 -*-
+# the page processor module of my spider
+# to process the page, extract url or other items
+
+class Pageprocessor:
+        def extractURLs(self):
+                pass
+
+if __name__ == '__main__':
+	pass
